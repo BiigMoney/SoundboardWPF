2f9a0033c5a8b4155528efdf0a873b671225f3cd
BiigMoney <64580434+BiigMoney@users.noreply.github.com>
2021-12-08 15:21:45 -0800

    new repo without secrets

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WMPLib;
+using System.Xml;
+using System.Globalization;
+
+namespace SoundboardWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private enum Window
+        {
+            MySounds,
+            AddSound,
+            BrowseSound,
+            Settings
+        }
+
+        public static WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+
+        private Window scene = Window.MySounds;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Main.Content = new MySounds();
+        }
+
+        public static void PlaySound(string url)
+        {
+            player.URL = url;
+            player.controls.play();
+        }
+
+        private void OpenMySounds(object sender, RoutedEventArgs e)
+        {
+            if(scene != Window.MySounds)
+            {
+                Main.Content = new MySounds();
+                scene = Window.MySounds;
+            }
+        }
+
+        private void OpenAddSound(object sender, RoutedEventArgs e)
+        {
+            if (scene != Window.AddSound)
+            {
+                Main.Content = new Add_Sound();
+                scene = Window.AddSound;
+            }
+        }
+
+        private void OpenBrowseSounds(object sender, RoutedEventArgs e)
+        {
+            if (scene != Window.BrowseSound)
+            {
+                Main.Content = new Browse_Sounds();
+                scene = Window.BrowseSound;
+            }
+        }
+
+        private void OpenSettings(object sender, RoutedEventArgs e)
+        {
+            if (scene != Window.Settings)
+            {
+                Main.Content = new Settings();
+                scene = Window.Settings;
+            }
+        }
+
+    }
+
+    public class Sound
+    {
+        public string Name { get; set; }
+        public String Length { get; set; }
+        public string Path { get; set; }
+
+        public Sound(string n, String l, string p)
+        {
+            Name = n;
+            Length = l;
+            Path = p;
+        }
+    }
+}
---
+++ MySounds.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using WMPLib;
+
+namespace SoundboardWPF
+{
+    /// <summary>
+    /// Interaction logic for MySounds.xaml
+    /// </summary>
+    public partial class MySounds : Page
+    {
+        XmlDocument doc = new XmlDocument();
+
+        public MySounds()
+        {
+            InitializeComponent();
+            doc.PreserveWhitespace = true;
+            doc.Load(@".\sounds.xml");
+            XmlNodeList sounds = doc.SelectNodes("/sounds/sound");
+            foreach (XmlNode item in sounds)
+            {
+                SoundList.Items.Add(new Sound(item.Attributes["name"].Value, item.Attributes["length"].Value, item.Attributes["path"].Value));
+            }
+        }
+        public void PlaySound(object sender, RoutedEventArgs e)
+        {
+            Sound sound = (sender as Button).DataContext as Sound;
+            MainWindow.PlaySound(sound.Path);
+
+        }
+        public void DeleteSound(object sender, RoutedEventArgs e)
+        {
+            Sound sound = (sender as Button).DataContext as Sound;
+            SoundList.Items.Remove(sound);
+            XmlNode node = doc.SelectSingleNode(String.Format("/sounds/sound[@path='{0}']", sound.Path));
+            node.ParentNode.RemoveChild(node);
+            doc.Save(@".\sounds.xml");
+        }
+
+    }
+}
