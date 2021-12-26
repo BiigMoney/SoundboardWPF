dd71012f60a197e9cc5a901000d6ae7cbd6c5e2d
BiigMoney <64580434+OKThomas1@users.noreply.github.com>
2021-12-26 04:04:53 -0800

    mvvm setup

---
+++ ViewModels/ShellViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundboardWPF.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        public async void OpenMySounds()
+        {
+            await ActivateItemAsync(new MySoundsViewModel());
+        }
+
+        public async void OpenAddSound()
+        {
+            await ActivateItemAsync(new AddSoundViewModel());
+        }
+
+        public async void OpenBrowseSounds()
+        {
+            await ActivateItemAsync(new BrowseSoundViewModel());
+        }
+
+        public async void OpenSettings()
+        {
+            await ActivateItemAsync(new SettingsViewModel());
+        }
+
+    }
+}
