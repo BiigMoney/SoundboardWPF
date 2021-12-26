fd5e542f00352ffaa3bda5de790db0a60769e3f7
BiigMoney <64580434+OKThomas1@users.noreply.github.com>
2021-12-26 04:21:39 -0800

    update csproj

--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -1,14 +1,55 @@
 ﻿using Caliburn.Micro;
 using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
 
 namespace SoundboardWPF.ViewModels
 {
     public class ShellViewModel : Conductor<object>
     {
+        XmlDocument doc = new XmlDocument();
+
+        public ArrayList MySounds = new ArrayList();
+
+        private Visibility _showEmpty = Visibility.Hidden;
+
+        public Visibility ShowEmpty
+        {
+            get { return _showEmpty; }
+            set { _showEmpty = value; }
+        }
+
+        private Visibility _showTable = Visibility.Hidden;
+
+        public Visibility ShowTable
+        {
+            get { return _showTable; }
+            set { _showTable = value; }
+        }
+
+        public ShellViewModel()
+        {
+            doc.PreserveWhitespace = true;
+            doc.Load(@".\sounds.xml");
+            XmlNodeList sounds = doc.SelectNodes("/sounds/sound");
+            foreach (XmlNode item in sounds)
+            {
+                MySounds.Add(new Sound(item.Attributes["name"].Value, item.Attributes["length"].Value, item.Attributes["path"].Value));
+            }
+
+            if(MySounds.Count > 0)
+            {
+                ShowTable = Visibility.Visible;
+            } else
+            {
+                ShowEmpty = Visibility.Visible;
+            }
+        }
+
         public async void OpenMySounds()
         {
             await ActivateItemAsync(new MySoundsViewModel());
@@ -27,6 +68,20 @@ namespace SoundboardWPF.ViewModels
         public async void OpenSettings()
         {
             await ActivateItemAsync(new SettingsViewModel());
+        }
+
+        public class Sound
+        {
+            public string Name { get; set; }
+            public String Length { get; set; }
+            public string Path { get; set; }
+
+            public Sound(string n, String l, string p)
+            {
+                Name = n;
+                Length = l;
+                Path = p;
+            }
         }
 
     }
